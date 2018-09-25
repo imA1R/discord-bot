8e0d371eef58db5b8a74f3aacd1062325057ab16
imA1R <43504639+imA1R@users.noreply.github.com>
2018-09-25 22:52:09 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -39,51 +39,17 @@ client.on('message', msg => {
 	
 });
 
-
-
-exports.run = (client, message, params) => {
-
-  if (!params[0]) {
-    const commandNames = Array.from(client.commands.keys());
-    const longest = commandNames.reduce((long, str) => Math.max(long, str.length), 0);
-    message.author.sendCode('asciidoc', `= Komut Listesi =\n\n[Komut hakkında bilgi için ${ayarlar.prefix}yardım <komut adı>]\n\n${client.commands.map(c => `${ayarlar.prefix}${c.help.name}${' '.repeat(longest - c.help.name.length)} :: ${c.help.description}`).join('\n')}`);
-  if (message.channel.type !== 'dm') {
-    const ozelmesajkontrol = new Discord.RichEmbed()
-    .setColor(0x00AE86)
-    .setTimestamp()
-    .setAuthor(message.author.username, message.author.avatarURL)
-    .setDescription('Özel mesajlarını kontrol et. :postbox:');
-    message.channel.sendEmbed(ozelmesajkontrol) }
-  } else {
-    let command = params[0];
-    if (client.commands.has(command)) {
-      command = client.commands.get(command);
-      message.author.sendCode('asciidoc', `= ${command.help.name} = \n${command.help.description}\nDoğru kullanım: ` + prefix + `${command.help.usage}`);
-    }
-  }
-
-};
-
-exports.conf = {
-  enabled: true,
-  guildOnly: false,
-  aliases: ['h', 'halp', 'help', 'y'],
-  permLevel: 0
-};
-
-exports.help = {
-  name: 'yardım',
-  description: 'Tüm komutları gösterir.',
-  usage: 'yardım [komut]'
-};
-
-
-	
-
-
-
-	
-	
+client.on("guildMemberAdd", member =>{
+	 
+	 var.channel = member.guild.channels.find("name", "giriş-çıkış");
+	 if (!channel) return;
+	 var role = member.guild.roles.find("name", "New Member");
+	 if (!role) return ;
+	 
+	 member.addRole(role);
+	 channel.sendMessage(member + role + "rütbesini aldı !");
+	 member.sendMessage("Hey Aramıza Hoş Geldin Dostum ! İlk Rütbeni Aldın Gelişmeye bak...")
+	 
  
 
   
