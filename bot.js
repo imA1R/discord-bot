389c06205bbb9e4e6a80af7dfc8e828855138a51
imA1R <43504639+imA1R@users.noreply.github.com>
2018-10-13 18:25:56 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1139,14 +1139,15 @@ client.on('message', msg => {
 client.on('message', msg => {
   if(msg.toLowerCase() === prefix +'sil 100') {
    msg.delete(100)
-   msg.sendMessage('100 msj temizlendi !');
+    msg.sendMessage('100 msj temizlendi !');
   }
 });
 	  
 client.on('message', msg => {
   if(msg.toLowerCase() === prefix +'sil 50') {
    msg.delete(50)
-   msg.sendMessage('50 msj temizlendi !');
+    msg.sendMessage('50 msj temizlendi !');  
+  }
 
 
 	   
