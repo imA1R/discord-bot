a7032aff6651318d83e69c1b3b444629c9c756f6
imA1R <43504639+imA1R@users.noreply.github.com>
2018-10-14 17:55:14 +0300

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -4,8 +4,6 @@ const ayarlar = require('../ayarlar.json');
 var prefix = ayarlar.prefix;
 
 module.exports = client => {
-  console.log BOT (Aktif, Komutlar yüklendi!`);
-  console.log BOT (${client.user.username} ismi ile giriş yapıldı!`);
   client.user.setStatus("online");
   client.user.setGame(`${prefix}yardım + ${client.guilds.size} sunucu + ${client.users.size} kullanıcı`);
   console.log BOT (Oyun ismi ayarlandı!`);
