cff2143d71fb71edf4ccb68cea8e859b92e410db
imA1R <43504639+imA1R@users.noreply.github.com>
2018-10-14 20:40:00 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -28,7 +28,7 @@ client.on("guildMemberAdd", member => {
 });
 
 client.on('message', msg => {
-  if (msg.content === prefix + 'ekle') {
+  if (msg.content.toLowerCase() === prefix + 'ekle') {
    msg.channel.sendMessage('https://discordapp.com/oauth2/authorize?client_id=492414649044697108&scope=bot&permissions=0')
 	  
 });
