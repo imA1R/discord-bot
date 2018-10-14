886f8b4a1500a116543111ca502b2fe008c60ba9
imA1R <43504639+imA1R@users.noreply.github.com>
2018-10-14 16:50:43 +0300

    Update guildCreate.js

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -5,17 +5,12 @@ const ayarlar = require('../ayarlar.json');
 var prefix = ayarlar.prefix;
 
 const girismesaj = [
-  '**Sohbet ve Oyun BOT sunucunuza eklendi!**',
-  '**Sohbet ve Oyun BOT** sunucunuzdaki insanlara kolaylıklar sağlar.',
-  'Bot Serhan E. tarafından geliştirilmektedir (https://serhan.pw)',
-  'Botumuzun özelliklerini öğrenmek için !yardım komutunu kullanabilirsin.',
-  '**ÖNEMLİ:** Botun kullanması için mod-log kanalı açın ve deneme için',
-  '!uyarı komutunu kullanın.',
-  '',
-  `**Sohbet ve Oyun BOT Resmî Discord Sunucusu** https://discord.gg/GvfuXmE`,
-  `**Sohbet ve Oyun Discord Sunucusu** https://discord.gg/GEeGjnH`
-]
-
+  '**Bot Sunucuna eklendi**',
+  '**Botumuz** sunucunuzdaki insanlara kolaylıklar sağlar.',
+  'Bot A1R tarafından geliştiriliyor',
+  
+  
+  
 client.on('guildCreate', guild => {
     const generalChannel = guild.defaultChannel
     generalChannel.sendMessage(girismesaj)
