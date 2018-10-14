e7a0558331f923edd61de6ce8e7f4fa43b6da0dd
imA1R <43504639+imA1R@users.noreply.github.com>
2018-10-14 17:06:12 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,8 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const ayarlar = require('./ayarlar.json');
+const fs = require('fs');
+require('./util/eventLoader')(client);
 
 
 var prefix =ayarlar.prefix
