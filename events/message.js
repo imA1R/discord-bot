b41d3337e30071f4fe916451f17449544fc66c95
imA1R <43504639+imA1R@users.noreply.github.com>
2018-10-14 20:59:56 +0300

    Update message.js

--- events/message.js
+++ events/message.js
@@ -15,11 +15,7 @@ module.exports = message => {
   let params = message.content.split(' ').slice(1);
   let perms = client.elevation(message);
   let cmd;
-  if (client.commands.has(command)) {
-    cmd = client.commands.get(command);
-  } else if (client.aliases.has(command)) {
-    cmd = client.commands.get(client.aliases.get(command));
-  }
+
   if (cmd) {
     if (perms < cmd.conf.permLevel) return;
     cmd.run(client, message, params, perms);
