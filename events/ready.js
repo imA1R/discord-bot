db073d6117737b9438b085b31300bdd83ba7cc69
imA1R <43504639+imA1R@users.noreply.github.com>
2018-10-14 18:04:50 +0300

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -4,8 +4,8 @@ const ayarlar = require('../ayarlar.json');
 var prefix = ayarlar.prefix;
 
 module.exports = client => {
-  client.user.setStatus("online");
+   client.user.setStatus("online");
   client.user.setGame(`${prefix}yardım + ${client.guilds.size} sunucu + ${client.users.size} kullanıcı`);
-  console.log BOT : (Oyun ismi ayarlandı!`);
-  console.log BOT :`(+ client.channels.size + ` adet kanala, ` + client.guilds.size + ` adet sunucuya ve ` + client.guilds.reduce((a, b) => a + b.memberCount, 0).toLocaleString() + ` kullanıcıya hizmet veriliyor!`);
+  console.log(` BOT: Oyun ismi ayarlandı!`);
+  console.log(` BOT: Şu an ` + client.channels.size + ` adet kanala, ` + client.guilds.size + ` adet sunucuya ve ` + client.guilds.reduce((a, b) => a + b.memberCount, 0).toLocaleString() + ` kullanıcıya hizmet veriliyor!`);
 };
