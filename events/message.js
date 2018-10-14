d999bf21df833bcd627aa4f6384876c89c935bf4
imA1R <43504639+imA1R@users.noreply.github.com>
2018-10-14 20:46:58 +0300

    Update message.js

--- events/message.js
+++ events/message.js
@@ -14,14 +14,6 @@ module.exports = message => {
   let command = message.content.split(' ')[0].slice(ayarlar.prefix.length);
   let params = message.content.split(' ').slice(1);
   let cmd;
-  if (client.commands.has(command)) {
-    cmd = client.commands.get(command);
-  } else if (client.aliases.has(command)) {
-    cmd = client.commands.get(client.aliases.get(command));
-  }
-  if (cmd) {
-    if (perms < cmd.conf.permLevel) return;
-    cmd.run(client, message, params, perms);
   }
 
 };
