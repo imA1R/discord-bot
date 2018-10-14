ab3c79a506fb4e1a82909d528e9cfa3533ba98ed
imA1R <43504639+imA1R@users.noreply.github.com>
2018-10-14 21:29:38 +0300

    Create ping.js

---
+++ komutlar/ping.js
@@ -0,0 +1,19 @@
+const Discord = require('discord.js');
+
+
+exports.run = function(client, message) {
+
+};
+
+exports.conf = {
+  enabled: true, //komutut açtık
+  guildOnly: false, //sadece servere özel yapmadık
+  aliases: ['ping','p'], //farklı çağrılar ekledik
+  permLevel: 0 //kimlerin kullanabileceğini yazdık (bot.js dosyasında perm leveller yazıyor)
+};
+
+exports.help = {
+  name: 'ping', //adını belirledik (kullanmak için gereken komut)
+  description: 'Botun pingini gösterir', //açıklaması
+  usage: 'ping' //komutun kullanım şekli (mesela hava <bölge>)
+};
