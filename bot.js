34b6a5dac5bb9de98013b09fd0b872491c14df27
imA1R <43504639+imA1R@users.noreply.github.com>
2018-10-21 23:29:24 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -74,16 +74,6 @@ client.unload = command => {
   });
 
 
-client.on('message', msg => {
-  if (msg.content.toLowerCase() === 'sa') {
-		if (!msg.guild.member(msg.author).hasPermission("BAN_MEMBERS")) {
-			msg.author.sendMessage('Aleyküm selam,  hoş geldin ^^'); 
-		} else {
-		msg.reply('Aleyküm selam, hoş geldin ^^');
-		}
-	}
-});
-
 
 
 client.on("guildMemberAdd", member => {
