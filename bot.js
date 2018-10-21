b67d94e6060dad59c8978e96dec610d1258c9e78
imA1R <43504639+imA1R@users.noreply.github.com>
2018-10-21 23:58:20 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -14,17 +14,7 @@ client.on('ready', () => {
 	      
 
 
-client.elevation = message => {
-  if(!message.guild) {
-	return; }
-  let permlvl = 0;
-  if (message.member.hasPermission("BAN_MEMBERS")) permlvl = 2;
-  if (message.member.hasPermission("ADMINISTRATOR")) permlvl = 3;
-  if (message.author.id === ayarlar.sahip) permlvl = 4;
-  return permlvl;
 
-	    
-});	    
  
 
 client.on("guildMemberAdd", member => {
